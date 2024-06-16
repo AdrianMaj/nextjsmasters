a1f109c498b95c386bad048867c64962c63b2b44
Adrian Maj <adrianmaj1122@gmail.com>
2024-06-16 20:08:32 +0200

    Added data attribute for testing

--- src/ui/organisms/ProductsList.tsx
+++ src/ui/organisms/ProductsList.tsx
@@ -5,7 +5,10 @@ import { type Product } from "@/types/productType";
 export const ProductsList = ({ productsList }: { productsList: Product[] }) => {
 	return (
 		<section className="bg-slate-300 p-10">
-			<ul className="flex h-dvh flex-wrap items-center justify-center gap-8">
+			<ul
+				data-testid="products-list"
+				className="flex h-dvh flex-wrap items-center justify-center gap-8"
+			>
 				{productsList.map((product) => (
 					<ProductsListItem key={product.id} product={product} />
 				))}
